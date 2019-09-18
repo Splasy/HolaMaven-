6b7d84be0e4ecdd18480f18953cae8b1c85c1b31
Aitor RM <aitorromojaromartinez@gmail.com>
2019-09-18 11:02:39 +0100

    first commit

---
+++ src/main/java/dad/maven/Main.java
@@ -0,0 +1,13 @@
+package dad.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String invertido = StringUtils.reverse("Hola Maven!");
+		System.out.println(invertido);
+		
+	}
+
+}
